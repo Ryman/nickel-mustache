4f7051b066ff8b4aff0c1727b96ba46803eee242
Kevin Butler <haqkrs@gmail.com>
2015-10-30 01:10:11 +0000

    docs(*): add example of changing caching behavior at runtime

---
+++ examples/example.rs
@@ -0,0 +1,49 @@
+#[macro_use] extern crate nickel;
+extern crate mustache;
+extern crate nickel_mustache;
+
+use nickel_mustache::{TemplateSupport, Render};
+use nickel::{Nickel, HttpRouter};
+use mustache::Template;
+
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::sync::RwLock;
+use std::env;
+
+type Cache = HashMap<PathBuf, Template>;
+
+struct ServerData {
+    use_cache: bool,
+    template_cache: RwLock<Cache>
+}
+
+impl TemplateSupport for ServerData {
+    type Cache = RwLock<Cache>;
+
+    fn cache(&self) -> Option<&Self::Cache> {
+        if self.use_cache {
+            Some(&self.template_cache)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    let data = ServerData {
+        use_cache: env::args().all(|arg| arg != "nocache"),
+        template_cache: RwLock::new(HashMap::new())
+    };
+
+    let mut server = Nickel::with_data(data);
+
+    server.get("/*", middleware! { |_req, res|
+        let mut data = HashMap::new();
+        data.insert("name", "World");
+
+        return Render::render(res, "examples/assets/my_template", &data)
+    });
+
+    server.listen("127.0.0.1:6767");
+}
